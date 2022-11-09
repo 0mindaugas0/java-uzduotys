7594ca50debbfadfb833a61e3274b2d4aba308f2
0mindaugas0 <mindaugas.mizaras@gmail.com>
2022-11-09 10:44:39 +0200

    pridetos uzduotys

--- src/AntraUzduotis.java
+++ src/AntraUzduotis.java
@@ -6,6 +6,16 @@ public class AntraUzduotis {
 
         long dviejuSandauga = sandauga(sk1, sk2);
         System.out.println(dviejuSandauga);
+
+
+        int sk3 = 15;
+        int sk4 = 25;
+
+        double dviejuDalyba = dalyba(sk3, sk4);
+        System.out.println(dviejuDalyba);
+
+        boolean arDalinasiIs5 = liekana(sk3);
+        System.out.println(arDalinasiIs5);
     }
     
     public static long sandauga(long x, long y){
@@ -13,4 +23,15 @@ public class AntraUzduotis {
         long atsakymas1 = atsakymas;
         return atsakymas1;
     }
+
+
+    public static double dalyba(double x, double y){
+        double atsakymas = x / y;
+        return atsakymas;
+    }
+
+    public static boolean liekana(int x){
+        return x % 5 == 0 && x % 3 == 0;
+    }
+
 }
