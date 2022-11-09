116dd72a33b8e05979e9591fe8d050ed5091493d
0mindaugas0 <mindaugas.mizaras@gmail.com>
2022-11-09 10:00:49 +0200

    naujas projektas

---
+++ src/AntraUzduotis.java
@@ -0,0 +1,16 @@
+public class AntraUzduotis {
+    public static void main(String[] args) {
+
+        long sk1 = 10;
+        long sk2 = 5;
+
+        long dviejuSandauga = sandauga(sk1, sk2);
+        System.out.println(dviejuSandauga);
+    }
+    
+    public static long sandauga(long x, long y){
+        long atsakymas = x * y;
+        long atsakymas1 = atsakymas;
+        return atsakymas1;
+    }
+}
